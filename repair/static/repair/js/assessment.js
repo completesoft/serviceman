63ecc1c14579fbe57084b6cd8709c3766f01a087
docent2010 <docent2010@ukr.net>
2017-12-01 15:14:37 +0200

    2017120101
    CHANGE: filtering in assessment

--- repair/static/repair/js/assessment.js
+++ repair/static/repair/js/assessment.js
@@ -9,16 +9,27 @@ $(document).ready(function(){
       ajaxRewardDel(this);
     });
 
-    var table = $('#assessment_table').DataTable();
+
+    var table = $('#assessment_table').DataTable({
+        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "ВСЕ"] ],
+        'pageLength': 25,
+        "columnDefs": [
+            {
+                'targets' : 0,
+                render: function ( data, type, row, meta ) {
+                    var dateSplit = data.split('.');
+                    return type === "sort" ? dateSplit[2] +'-'+ dateSplit[1] +'-'+ dateSplit[0] : data;
+                }
+            }
+        ]
+    });
+
 
     table.columns().every( function () {
         var that = this;
-
         $( 'input', this.footer() ).on( 'keyup change', function () {
             if ( that.search() !== this.value ) {
-                that
-                    .search( this.value )
-                    .draw();
+                that.search( this.value ).draw();
             }
         } );
     } );
